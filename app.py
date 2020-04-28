d65d995b5bb47ff0bb598026e748dea252b390fa
Alan <alannavaseg_02@hotmail.com>
2020-04-28 12:53:57 -0500

    Reload in heroku

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ Bootstrap(app)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html') 
 
 if __name__ == '__main__':
     app.run(debug=True)
